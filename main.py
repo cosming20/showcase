d38d48839659fe75ee999759344ea4cf7e098018
Cosmin Gagea <cosmin.ionut.gagea@gmail.com>
2024-03-25 18:40:44 +0200

    gallery

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
-from flask import Flask, render_template,url_for,jsonify, send_file
+from flask import Flask, render_template,url_for,jsonify, send_file, request
 from pymongo import MongoClient
 from gridfs import GridFS
 from PIL import Image
 import io
 import json
-client = MongoClient("")
+from bson import json_util, ObjectId
+import base64
 
+# https://imgur.com/a/Tn38oJ5
 
 client = MongoClient('mongodb+srv://cosminionutgagea:barbosuetare@cluster0.zcscweb.mongodb.net/')
 db = client['PythonProject']
 photos = db['photos']
 videos = db['videos']
-fs = GridFS(db)
+
 
 app = Flask(__name__)
-with open ("image.jpg", "rb") as file:
-    image_data = file.read()
-
-
-file_id = fs.put(image_data, filename = "image.jpg")
-
-retrieved_image = fs.get(file_id)
-image = Image.open(io.BytesIO(retrieved_image.read()))
-image.show()
 
 @app.route('/')
 def main_page():
     return render_template('home.html')
 
+@app.route('/galerie')
+def show_gallery():
+    return render_template('galerie.html')
+    
+
 @app.route('/cascada')
 def gallery_page():
     return render_template('gallery.html')
 
-@app.route('/api/media')
-def get_media():
-    # Retrieve photos and videos from MongoDB
-    media = list(db.media.find({}))
-    return jsonify(media)
+@app.route('/image_details/<image_id>')
+def image_details(image_id):
+    # Your logic to retrieve details of the image with the given ID
+    # This could involve querying your database or performing other operations
+    # For demonstration purposes, let's just return a simple message
+    return f"Details for image with ID {image_id}"
 
-@app.route('/media/<media_id>')
-def get_media_file(media_id):
-    # Retrieve the media file from MongoDB GridFS
-    media_file = fs.get(media_id)
-    # return send_file(media_file, mimetype=media_file.content_type)
-    return send_file(io.BytesIO(media_file.read()), mimetype=media_file.content_type)
+@app.route('/galerie')
+def galerie():
+    return render_template('galerie.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
